63b2fcf80bb201446bf8d42661d9c824597b51c7
Belén Albeza <belen.albeza@gmail.com>
2018-08-12 10:44:26 +0200

    add missing files

---
+++ main/play/membank.go
@@ -0,0 +1,50 @@
+components {
+  id: "map"
+  component: "/main/play/membank.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "controller"
+  component: "/main/play/membank.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/assets/images/level.atlas\"\n"
+  "default_animation: \"bank\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 256.0
+    y: 256.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
